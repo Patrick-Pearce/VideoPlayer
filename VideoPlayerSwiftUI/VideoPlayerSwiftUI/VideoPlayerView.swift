1a6fe79b8a204c92cce55b65881700a38719ed8d
Patrick Renzo Pearce <patrick_pearce@outlook.com>
2024-01-15 14:26:13 -0500

    Added a fetchVideos function to test the video service and obtain the JSON from the server

--- VideoPlayerSwiftUI/VideoPlayerSwiftUI/VideoPlayerView.swift
+++ VideoPlayerSwiftUI/VideoPlayerSwiftUI/VideoPlayerView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import Combine
 
 struct VideoPlayerView: View {
+    @State private var videos: [Video] = []
+    @State private var currentVideo: Video?
+    @State private var cancellables: Set<AnyCancellable> = []
+    
     var body : some View {
         // the title header of the app
-        Rectangle()
-            .fill(Color.black)
-            .overlay(
-                Text("Video Player")
-                    .font(Font.system(size: 30.0))
-                    .foregroundColor(.white)
-            )
+        VStack(spacing: 0) {
+            Rectangle()
+                .fill(Color.black)
+                .overlay(
+                    Text("Video Player")
+                        .font(Font.system(size: 30.0))
+                        .foregroundColor(.white)
+                )
+                .onAppear {
+                    fetchVideos()
+                }
+        }
+    }
+    
+    private func fetchVideos() {
+        VideoService.shared.fetchVideosInfo()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(let error):
+                        print("Error fetching videos: \(error)")
+                }
+            }, receiveValue: { videos in
+                self.videos = videos
+                self.currentVideo = videos.first
+                print(videos)
+
+            })
+            .store(in: &cancellables)
     }
 }
